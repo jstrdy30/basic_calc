557e66d9846811d90774584cf0d904bc4f9ac2e4
j.strdy <janmarksabado698@gmail.com>
2024-08-24 19:25:32 +0800

    Add files via upload

---
+++ Simple Calculator.py
@@ -0,0 +1,35 @@
+#Python project | Simple CalcuLATER
+
+import time
+
+#Operations
+print("1.Addition")
+print("2.Subtraction")
+print("3.Multiplication")
+print("4.Division")
+print("5.Exit")
+choice = int(input("\nEnter your choice: "))
+
+#Choosing a number
+if choice in (1,2,3,4):
+    choice_1 = float(input("\nEnter your first number: "))
+    choice_2 = float(input("Enter your second number: "))
+    
+    if choice == 1:
+        print(f"\n{choice_1} + {choice_2} = {choice_1 + choice_2}")
+    elif choice == 2:
+        print(f"\n{choice_1} - {choice_2} = {choice_1 - choice_2}")
+    elif choice == 3:
+        print(f"\n{choice_1} × {choice_2} = {choice_1 * choice_2}")
+    elif choice == 4:
+        print(f"\n{choice_1} ÷ {choice_2} = {choice_1 / choice_2}")
+
+elif choice == 5:
+    for i in range(3,0,-1):
+        print(f"Code will end in: {i}")
+        time.sleep(1)
+    
+        
+        
+else:
+    print("Invalid Input!!!")
